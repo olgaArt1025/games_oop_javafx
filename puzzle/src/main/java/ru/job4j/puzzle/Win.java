dff31786d4e383572c7eb66d2849fde99e176cf4
Olga_ <olga_art1025@mail.ru>
2021-10-03 00:54:52 +0500

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -15,7 +15,7 @@ public class Win {
     public static boolean monoVertical(int[][] board, int cell) {
         boolean result = true;
         for (int[] ints : board) {
-            if ((ints[cell] != 1)) {
+            if (ints[cell] != 1) {
                 result = false;
                 break;
             }
@@ -26,7 +26,7 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (monoHorizontal(board, i) || monoVertical(board, i)) {
+            if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
                 rsl = true;
                 break;
             }
